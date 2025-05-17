92548373fc5976ff591b8d9d1fe33760945ca1c6
Gabriel Nwankwo <gfunzy@gmail.com>
2025-05-17 15:06:31 +0100

    Done

--- resources/views/passenger/route_prices.blade.php
+++ resources/views/passenger/route_prices.blade.php
@@ -12,6 +12,12 @@
     <link rel="stylesheet" href="{{ asset('assets/css/style-blue.css') }}?ver={{ date('his') }}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
         integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+        <style type="text/css">
+            .nobreak {
+                white-space: nowrap;
+            }
+        </style>
 </head>
 
 <body class="admin-dashboard page-user">
