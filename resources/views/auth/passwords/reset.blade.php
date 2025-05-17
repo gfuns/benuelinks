ab239381634fa96c79eff5e3cee30b6a97d545b8
Gabriel Nwankwo <gfunzy@gmail.com>
2025-05-17 14:58:24 +0100

    Done

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -61,7 +61,7 @@
             }
 
             .page-ath-form{
-                margin: 30px 0px 250px 0px;
+                margin: 30px 0px 350px 0px;
             }
 
             .page-ath-form,
