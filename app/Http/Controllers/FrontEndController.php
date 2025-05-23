99274223f57cadddedd7facab70931ab5d777e0e
Gabriel Nwankwo <gfunzy@gmail.com>
2025-05-23 14:24:31 +0100

    FIXES

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -297,8 +297,6 @@ class FrontEndController extends Controller
         //     return back();
         // }
 
-        dd($request->all());
-
         $triptype    = request()->triptype;
         $departure   = request()->takeoff;
         $destination = request()->destination;
