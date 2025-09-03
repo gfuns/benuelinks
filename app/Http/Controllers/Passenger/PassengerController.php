af36af1b739bc54b255fc6d0f598507b0f88d739
Gabriel Nwankwo <gfunzy@gmail.com>
2025-09-03 16:05:23 +0100

    Trouble Shooting Travel Schedule

--- app/Http/Controllers/Passenger/PassengerController.php
+++ app/Http/Controllers/Passenger/PassengerController.php
@@ -218,6 +218,9 @@ class PassengerController extends Controller
             "deposits" => BankOneHelper::accountBalance(),
             "bonuses"  => 0,
         ];
+
+        dd($balances);
+
         $transactions = WalletTransactions::orderBy("id", "desc")->where("user_id", Auth::user()->id)->get();
         return view("passenger.wallet", compact("transactions", "balances"));
     }
