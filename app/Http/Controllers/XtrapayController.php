25d9e4e011361d5454988a4f786e520d61a4cd42
Gabriel Nwankwo <gfunzy@gmail.com>
2025-10-03 12:08:05 +0100

    Modifying App

--- app/Http/Controllers/XtrapayController.php
+++ app/Http/Controllers/XtrapayController.php
@@ -50,11 +50,9 @@ class XtrapayController extends Controller
             $webhook->payload               = json_encode($request->all());
             if ($webhook->save()) {
 
-                $xtraPayment = XtrapayPayments::where("reference", $data->reference)->where('handled', 0)->first();
+                if ($data->event == "virtual_account.transaction.success") {
 
-                if (isset($xtraPayment)) {
-
-                    if ($xtraPayment->trx_type == "topup") {
+                    if ($data->reference == null) {
 
                         try {
                             $user = User::where("account_number", $webhook->account_number)->first();
@@ -64,7 +62,7 @@ class XtrapayController extends Controller
                             $topup                 = new WalletTransactions;
                             $topup->user_id        = $user->id;
                             $topup->trx_type       = "credit";
-                            $topup->reference      = $data->reference;
+                            $topup->reference      = $data->reference ?? self::genInternalRef($user->id);
                             $topup->amount         = $webhook->amount;
                             $topup->balance_before = $user->wallet_balance;
                             $topup->balance_after  = ($webhook->amount + $user->wallet_balance);
@@ -86,122 +84,180 @@ class XtrapayController extends Controller
                         } catch (\Throwable $e) {
                             DB::rollback();
                             Log::channel('bankone')->error($e->getMessage());
-                        }
-
-                    }
-
-                    if ($xtraPayment->trx_type == "booking") {
-
-                        try {
-                            DB::beginTransaction();
-
-                            if ($xtraPayment->status == "success") {
-                                $xtraPayment->handled = 1;
-                                $xtraPayment->status  = "successful";
-                                $xtraPayment->save();
-
-                                $booking                  = TravelBooking::find($xtraPayment->transaction_id);
-                                $booking->payment_status  = "paid";
-                                $booking->payment_channel = "transfer";
-                                $booking->booking_status  = "booked";
-                                $booking->save();
-                            } else {
-                                $xtraPayment->handled = 1;
-                                $xtraPayment->status  = "failed";
-                                $xtraPayment->save();
-
-                                $booking                 = TravelBooking::find($xtraPayment->transaction_id);
-                                $booking->payment_status = "failed";
-                                $booking->save();
-                            }
-
-                            DB::commit();
-
-                            if ($xtraPayment->status == "success") {
-                                Mail::to($booking)->send(new BookingSuccessful($booking));
-                            }
-
                             return new JsonResponse([
-                                'received' => true,
+                                'received' => false,
+                                'message'  => $e->getMessage(),
                             ]);
-                        } catch (\Throwable $e) {
-                            DB::rollback();
-                            Log::channel('bankone')->error($e->getMessage());
                         }
 
                     }
 
-                    if ($xtraPayment->trx_type == "guest") {
-                        try {
-                            DB::beginTransaction();
+                    $xtraPayment = XtrapayPayments::where("reference", $data->reference)->where('handled', 0)->first();
 
-                            if ($xtraPayment->status == "success") {
+                    if (isset($xtraPayment)) {
 
-                                $xtraPayment->handled = 1;
-                                $xtraPayment->status  = "successful";
-                                $xtraPayment->save();
+                        if ($xtraPayment->trx_type == "booking") {
 
-                                $guest                  = GuestBooking::find($xtraPayment->transaction_id);
-                                $guest->payment_status  = "paid";
-                                $guest->payment_channel = "transfer";
-                                $guest->booking_status  = "booked";
-                                $guest->save();
+                            try {
+                                DB::beginTransaction();
 
-                                $booking                  = new TravelBooking;
-                                $booking->schedule_id     = $guest->schedule_id;
-                                $booking->departure       = $guest->departure;
-                                $booking->destination     = $guest->destination;
-                                $booking->vehicle         = $guest->vehicle;
-                                $booking->vehicle_type    = $guest->vehicle_type;
-                                $booking->travel_date     = $guest->travel_date;
-                                $booking->departure_time  = $guest->departure_time;
-                                $booking->full_name       = $guest->full_name;
-                                $booking->phone_number    = $guest->phone_number;
-                                $booking->nok             = $guest->nok;
-                                $booking->nok_phone       = $guest->nok_phone;
-                                $booking->email           = $guest->email;
-                                $booking->gender          = $guest->gender;
-                                $booking->seat            = $guest->seat;
-                                $booking->payment_channel = $guest->payment_channel;
-                                $booking->classification  = "booking";
-                                $booking->payment_status  = $guest->payment_status;
-                                $booking->travel_fare     = $guest->travel_fare;
-                                $booking->booking_number  = $guest->booking_number;
-                                $booking->booking_method  = "online";
-                                $booking->booking_status  = $guest->booking_status;
-                                $booking->save();
-                            } else {
-                                $xtraPayment->handled = 1;
-                                $xtraPayment->status  = "failed";
-                                $xtraPayment->save();
+                                if ($data->status == "success") {
+                                    $xtraPayment->handled = 1;
+                                    $xtraPayment->status  = "successful";
+                                    $xtraPayment->save();
 
-                                $guest                 = GuestBooking::find($xtraPayment->transaction_id);
-                                $guest->payment_status = "failed";
-                                $guest->save();
+                                    $booking                  = TravelBooking::find($xtraPayment->transaction_id);
+                                    $booking->payment_status  = "paid";
+                                    $booking->payment_channel = "transfer";
+                                    $booking->booking_status  = "booked";
+                                    $booking->save();
+                                } else {
+                                    $xtraPayment->handled = 1;
+                                    $xtraPayment->status  = "failed";
+                                    $xtraPayment->save();
+
+                                    $booking                 = TravelBooking::find($xtraPayment->transaction_id);
+                                    $booking->payment_status = "failed";
+                                    $booking->save();
+                                }
+
+                                DB::commit();
+
+                                if ($data->status == "success") {
+                                    Mail::to($booking)->send(new BookingSuccessful($booking));
+                                }
+
+                                return new JsonResponse([
+                                    'received' => true,
+                                ]);
+                            } catch (\Throwable $e) {
+                                DB::rollback();
+                                Log::channel('bankone')->error($e->getMessage());
                             }
 
-                            DB::commit();
-
-                            if ($xtraPayment->status == "success") {
-                                Session::forget('guestBookingID');
-                                Mail::to($booking)->send(new BookingSuccessful($booking));
-                            }
-
-                            return new JsonResponse([
-                                'received' => true,
-                            ]);
-
-                        } catch (\Throwable $e) {
-                            DB::rollback();
-                            Log::channel('bankone')->error($e->getMessage());
                         }
 
+                        if ($xtraPayment->trx_type == "guest") {
+                            try {
+                                DB::beginTransaction();
+
+                                if ($data->status == "success") {
+
+                                    $xtraPayment->handled = 1;
+                                    $xtraPayment->status  = "successful";
+                                    $xtraPayment->save();
+
+                                    $guest                  = GuestBooking::find($xtraPayment->transaction_id);
+                                    $guest->payment_status  = "paid";
+                                    $guest->payment_channel = "transfer";
+                                    $guest->booking_status  = "booked";
+                                    $guest->save();
+
+                                    $booking                  = new TravelBooking;
+                                    $booking->schedule_id     = $guest->schedule_id;
+                                    $booking->departure       = $guest->departure;
+                                    $booking->destination     = $guest->destination;
+                                    $booking->vehicle         = $guest->vehicle;
+                                    $booking->vehicle_type    = $guest->vehicle_type;
+                                    $booking->travel_date     = $guest->travel_date;
+                                    $booking->departure_time  = $guest->departure_time;
+                                    $booking->full_name       = $guest->full_name;
+                                    $booking->phone_number    = $guest->phone_number;
+                                    $booking->nok             = $guest->nok;
+                                    $booking->nok_phone       = $guest->nok_phone;
+                                    $booking->email           = $guest->email;
+                                    $booking->gender          = $guest->gender;
+                                    $booking->seat            = $guest->seat;
+                                    $booking->payment_channel = $guest->payment_channel;
+                                    $booking->classification  = "booking";
+                                    $booking->payment_status  = $guest->payment_status;
+                                    $booking->travel_fare     = $guest->travel_fare;
+                                    $booking->booking_number  = $guest->booking_number;
+                                    $booking->booking_method  = "online";
+                                    $booking->booking_status  = $guest->booking_status;
+                                    $booking->save();
+                                } else {
+                                    $xtraPayment->handled = 1;
+                                    $xtraPayment->status  = "failed";
+                                    $xtraPayment->save();
+
+                                    $guest                 = GuestBooking::find($xtraPayment->transaction_id);
+                                    $guest->payment_status = "failed";
+                                    $guest->save();
+                                }
+
+                                DB::commit();
+
+                                if ($data->status == "success") {
+                                    Session::forget('guestBookingID');
+                                    Mail::to($booking)->send(new BookingSuccessful($booking));
+                                }
+
+                                return new JsonResponse([
+                                    'received' => true,
+                                ]);
+
+                            } catch (\Throwable $e) {
+                                DB::rollback();
+                                Log::channel('bankone')->error($e->getMessage());
+                                return new JsonResponse([
+                                    'received' => false,
+                                    'message'  => $e->getMessage(),
+                                ]);
+                            }
+
+                        }
+                    } else {
+                        return new JsonResponse([
+                            'received' => false,
+                            'message'  => "Something went wrong identifying transaction",
+                        ]);
                     }
+                } else {
+                    return new JsonResponse([
+                        'received' => true,
+                    ]);
                 }
+            } else {
+                Log::channel('bankone')->info("Webhook refused to save");
+                return new JsonResponse([
+                    'received' => false,
+                    'message'  => "Something went wrong Logging Webhook",
+                ]);
             }
 
         } catch (\Throwable $e) {
             Log::channel('bankone')->error($e->getMessage());
+            return new JsonResponse([
+                'received' => false,
+                'message'  => $e->getMessage(),
+            ]);
         }
+    }
+
+    /**
+     * genInternalRef
+     *
+     * @return void
+     */
+    public function genInternalRef($clientId)
+    {
+        // Get the current timestamp
+        $timestamp = (string) (strtotime('now') . microtime(true));
+
+        // Remove any non-numeric characters (like dots)
+        $cleanedTimestamp = preg_replace('/[^0-9]/', '', $timestamp);
+
+        $microtime = substr($cleanedTimestamp, -4);
+
+        // Shuffle the digits
+        $shuffled = str_shuffle($cleanedTimestamp);
+
+        // Extract the first 5 characters
+        $code = substr($shuffled, 0, 11);
+
+        $reference = $clientId . $microtime . $code;
+
+        return substr($reference, 0, 12);
     }
 }
